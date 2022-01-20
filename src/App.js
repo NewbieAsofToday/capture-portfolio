fd0af8698bc1c93379c45cad8366c0d091f61019
Azk <azkriven17@gmail.com>
2022-01-20 13:53:07 +0800

    9. Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import AboutSection from "./components/AboutSection";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "../src/pages/ContactUs";
+import OurWork from "../src/pages/OurWork";
+import { Switch, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Routes>
+        <Route path='/' element={<AboutUs />}></Route>
+        <Route path='/work' element={<OurWork />}></Route>
+        <Route path='/contact' element={<ContactUs />}></Route>
+      </Routes>
     </div>
   );
 }
