95aac59512c17422aefe4fa331bdf55f56fe1515
Azk <azkriven17@gmail.com>
2022-01-21 14:22:16 +0800

    16. Our Work Animation

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -5,7 +5,15 @@ import goodtimes from "../img/goodtimes-small.png";
 import styled from "styled-components";
 import { Routes, Link } from "react-router-dom";
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import {
+  pageAnimation,
+  fade,
+  photoAnimation,
+  lineAnimation,
+  sliderContainer,
+  slider,
+} from "../animation";
+
 const OurWork = () => {
   return (
     <motion.div
@@ -16,27 +24,43 @@ const OurWork = () => {
       exit='exit'
     >
       <Work>
+        <motion.div variants={sliderContainer}>
+          <Frame1 variants={slider}></Frame1>
+          <Frame2 variants={slider}></Frame2>
+          <Frame3 variants={slider}></Frame3>
+          <Frame4 variants={slider}></Frame4>
+        </motion.div>
         <Movie>
-          <h2>The Athlete</h2>
-          <div className='line'></div>
+          <motion.h2 variants={fade}>The Athlete</motion.h2>
+          <motion.div variants={lineAnimation} className='line'></motion.div>
           <Link to='/work/the-athlete'>
-            <img src={athlete} alt='athlete' />
+            <Hide>
+              <motion.img
+                variants={photoAnimation}
+                src={athlete}
+                alt='athlete'
+              />
+            </Hide>
           </Link>
         </Movie>
 
         <Movie>
           <h2>The Racer</h2>
-          <div className='line'></div>
+          <motion.div variants={lineAnimation} className='line'></motion.div>
           <Link to='/work/the-racer'>
-            <img src={theracer} alt='theracer' />
+            <Hide>
+              <img src={theracer} alt='theracer' />
+            </Hide>
           </Link>
         </Movie>
 
         <Movie>
           <h2>Good Times</h2>
-          <div className='line'></div>
+          <motion.div variants={lineAnimation} className='line'></motion.div>
           <Link to='/work/good-times'>
-            <img src={goodtimes} alt='goodtimes' />
+            <Hide>
+              <img src={goodtimes} alt='goodtimes' />
+            </Hide>
           </Link>
         </Movie>
       </Work>
@@ -55,7 +79,7 @@ const Movie = styled.div`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background-color: #ccc;
+    background-color: #23d997;
     margin-bottom: 3rem;
   }
   img {
@@ -64,5 +88,25 @@ const Movie = styled.div`
     object-fit: cover;
   }
 `;
-
+const Hide = styled.div`
+  overflow: hidden;
+`;
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background-color: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`;
 export default OurWork;
